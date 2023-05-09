13078914496f8377ebea7c34f9145379f85bfdd3
Skilly146 <third.landon.davidson4@gmail.com>
2023-05-08 22:09:47 -0700

    Finish

---
+++ CaesarsCipher.java
@@ -0,0 +1,65 @@
+public class CaesarsCipher implements Cipher {
+    private int shift;
+    public CaesarsCipher(int shift) {
+        setShift(shift);
+    }
+    public void setShift(int shift) {this.shift = shift % 26;}
+
+    @Override
+    public String encode(String plainText) {
+        String[] words = plainText.split(" ");
+        StringBuilder cipherText = new StringBuilder();
+        for (String w : words) {
+            for (int i = 0; i < w.length(); i++) {
+                char c = w.charAt(i);
+                if (!Character.isAlphabetic(c)) {cipherText.append(c);continue;}
+                if (Character.isUpperCase(c)) {
+                    c += shift;
+                    if (c < 65) {c += 26;}
+                    c -= 65;
+                    c %= 26;
+                    c += 65;
+                }
+                else {
+                    c += shift;
+                    if (c < 97) {c += 26;}
+                    c -= 97;
+                    c %= 26;
+                    c += 97;
+                }
+                cipherText.append(c);
+            }
+            cipherText.append(' ');
+        }
+        return cipherText.toString();
+    }
+
+    @Override
+    public String decode(String cipherText) {
+        String[] words = cipherText.split(" ");
+        StringBuilder plainText = new StringBuilder();
+        for (String w : words) {
+            for (int i = 0; i < w.length(); i++) {
+                char c = w.charAt(i);
+                if (!Character.isAlphabetic(c)) {plainText.append(c);continue;}
+                if (Character.isUpperCase(c)) {
+                    c -= shift;
+                    if (c < 65) {c += 26;}
+                    c -= 65;
+                    c %= 26;
+                    c += 65;
+                }
+                else {
+                    c -= shift;
+                    if (c < 97) {c += 26;}
+                    c -= 97;
+                    c %= 26;
+                    c += 97;
+                }
+                plainText.append(c);
+            }
+            plainText.append(' ');
+        }
+        return plainText.toString();
+    }
+}
---
+++ PigLatin.java
@@ -0,0 +1,43 @@
+public class PigLatin implements Cipher {
+    String suffix;
+    public PigLatin(String suffix) {
+        setSuffix(suffix);
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+    private boolean isVowel(char c) {
+        return "AEIOUaeiou".contains(String.valueOf(c));
+    }
+    @Override
+    public String encode(String plainText) {
+        String[] words = plainText.toLowerCase().split(" ");
+        StringBuilder cipherText = new StringBuilder();
+        for (String w : words) {
+            for (int i = 0; i < w.length(); i++) {
+                char c = w.charAt(i);
+                if (!Character.isAlphabetic(c)) {w = w.replace(Character.toString(c), "");}
+            }
+            int divider = isVowel(w.charAt(0)) ? 0 : 1;
+            String end = w.substring(0, divider) + suffix;
+            String rem = w.substring(divider);
+            cipherText.append(rem).append(end).append(" ");
+        }
+        return cipherText.toString();
+    }
+
+    @Override
+    public String decode(String cipherText) {
+        String[] words = cipherText.split(" ");
+        StringBuilder plainText = new StringBuilder();
+        for (String w : words) {
+            w = w.replace(suffix, "");
+            if (!isVowel(w.charAt(w.length() - 1))) {
+                w = w.charAt(w.length() - 1) + w.substring(0, w.length() - 1);
+            }
+            plainText.append(w).append(" ");
+        }
+        return plainText.toString();
+    }
+}
